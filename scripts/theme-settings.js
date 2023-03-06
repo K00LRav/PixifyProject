3b0d513bfd9b905ffb3c1bc04f685ac9c8b1058b
Rav <raymonds@Ravs-Mac-mini.attlocal.net>
2023-03-05 20:10:04 -0800

    made changes to css and js script

--- scripts/theme-settings.js
+++ scripts/theme-settings.js
@@ -5,14 +5,19 @@ const toggleTheme=()=>{
 
   const body = document.querySelector("body")
   const div = document.querySelector("div")
+  const subBtn = document.querySelector(".sub-btn")
+  const prevBtn = document.getElementById("previous-btn")
+
 
   if(body.style.backgroundColor==="white"){
     body.style.backgroundColor="#2A3B49"
     div.style.color ="white"
+    subBtn.style.border = "2px solid #7BFFE9"
     themeButton.innerHTML="Dark Mode"
   }else{
     body.style.backgroundColor="white"
     div.style.color = "black"
+    subBtn.style.border ="#7BFFE9"
     themeButton.innerHTML="Light Mode"
   }
 
